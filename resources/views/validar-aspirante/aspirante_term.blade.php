607ef16d5e9371fa414b960734e35fbe05bfd7e8
juan.diaz <rulos26@gmail.com>
2025-01-09 10:30:43 -0500

    borrado
    borrado

--- resources/views/validar-aspirante/aspirante_term.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('adminlte::page')
-@section('template_title')
-    {{ __('Create') }} Busqueda
-@endsection
-
-@section('content')
- informe terminado
-@endsection
-@include('sidebar.right-sidebar')
